bae4d59275cab0e8415ec4af4a8adaee736d85b1
Courtnee McCollum <courtneemeaghan@gmail.com>
2021-07-22 14:38:46 -0500

    Allow user to drag balls anywhere on screen

--- src/index.js
+++ src/index.js
@@ -4,9 +4,11 @@ function onDragStart() {
   setTimeout(() => (this.classList.add('invicible', 'dragging')), 0)
 }
 
-function onDragEnd() {
-  console.log(this);
-  this.classList.remove('invicible');
+function onDragEnd(e) {
+  this.classList.remove('invicible', 'dragging');
+  this.style.position = 'absolute';
+  this.style.top = `${e.clientY - 82}px`;
+  this.style.left = `${e.clientX - 35}px`;
 }
 
 balls.forEach(ball => {
@@ -14,7 +16,8 @@ balls.forEach(ball => {
   ball.addEventListener('dragend', onDragEnd);
 });
 
-// enter
-// leave
-// over
-// drop
+function onDragOver(e) {
+  e.preventDefault();
+}
+
+document.querySelector('body').addEventListener('dragover', onDragOver);
